2fb001bd427c7e6359d6397c167964a003257c19
madanghimire-3ptrades <madanghimire-3ptrades@3ptrades.com>
2025-06-17 18:59:13 +0545

    chore add winston logger in app

--- src/infrastructure/logger.ts
+++ src/infrastructure/logger.ts
@@ -6,11 +6,13 @@ import fs from "fs";
 const logDir = path.resolve("logs");
 if (!fs.existsSync(logDir)) fs.mkdirSync(logDir);
 
+const { combine, timestamp, printf } = winston.format;
+
 const logger = winston.createLogger({
   level: "info",
-  format: winston.format.combine(
-    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-    winston.format.printf(({ level, message, timestamp }) => {
+  format: combine(
+    timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    printf(({ level, message, timestamp }) => {
       return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
     })
   ),
@@ -23,6 +25,16 @@ const logger = winston.createLogger({
       filename: path.join(logDir, "combined.log"),
     }),
     new winston.transports.Console(),
+  ],
+  exceptionHandlers: [
+    new winston.transports.File({
+      filename: path.join(logDir, "exception.log"),
+    }),
+  ],
+  rejectionHandlers: [
+    new winston.transports.File({
+      filename: path.join(logDir, "rejections.log"),
+    }),
   ],
 });
 
